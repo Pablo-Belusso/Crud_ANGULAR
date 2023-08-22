b4212dd555942a9fae73a24cdaf7f28d0d83716e
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-08-22 11:54:45 -0300

    Commit 38

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 
 export class ProductCreateComponent implements OnInit {
 
-  atributoLegal = "qualquer"
+  propLegal = "qualquer"
 
   constructor() { }
 
