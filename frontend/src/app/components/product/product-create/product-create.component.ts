acc7c6c02ca6a368f01546ba6ab7a2e8ba184521
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-08-22 10:17:52 -0300

    Commit 36

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
   styleUrls: ['./product-create.component.css']
 })
-export class ProductCreateComponent {
+
+export class ProductCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+      
+  }
+
+  fazerAlgo(): void {
+    console.log('Fazendo Algo');      
+  }
 
 }
