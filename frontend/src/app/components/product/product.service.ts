4777c78964c9573d088c0a16d98c177245cbad93
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-08-28 19:44:56 -0300

    Commit 49

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,11 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Product } from './product.model';
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class ProductService {
+
+  baseUrl = "http://localhost:3001/products"
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
@@ -15,6 +20,10 @@ export class ProductService {
       horizontalPosition: "right",
       verticalPosition: "top"
     })
+  }
+
+  create(product: Product) {
+    return this.http.post(this.baseUrl, product)
   }
 
 }
