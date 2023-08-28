b477cb29ea5fa690e783aa7d7136e703612c087e
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-08-27 23:53:22 -0300

    Commit 48

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
@@ -6,7 +7,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class ProductService {
 
-  constructor(private snackBar: MatSnackBar) { }
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
   showMessage(msg: string): void {
     this.snackBar.open(msg, 'X', {
