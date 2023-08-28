3e93cb1bdc5c674ef1b7638937b6805d8ccc4308
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-08-27 23:42:01 -0300

    Commit 46

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -9,7 +9,7 @@ export class ProductService {
   constructor(private snackBar: MatSnackBar) { }
 
   showMessage(msg: string): void {
-    this.snackBar.open(msg, 'undo', {
+    this.snackBar.open(msg, 'X', {
       duration: 3000,
       horizontalPosition: "right",
       verticalPosition: "top"
