ddfe38c8a0bc0183ee975887b48c109a86f9f839
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-08-30 13:41:00 -0300

    Commit 55

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -22,6 +22,11 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { HttpClientModule } from '@angular/common/http';
 
+import '@angular/forms';
+import '@angular/material/form-field';
+import '@angular/material/input';
+
+
 @NgModule({
   declarations: [
     AppComponent,
