ac09a3d6e0ba80b7a8ce4c24fcd9e7403f03bbb9
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-09-05 15:45:44 -0300

    Commit 66

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -1,4 +1,6 @@
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-read',
@@ -8,10 +10,17 @@ import { Component, OnInit } from '@angular/core';
 
 export class ProductReadComponent implements OnInit {
 
-  constructor() { }
+  products: Product[]
+
+  constructor(private productService: ProductService) { 
+    this.products = []; // Inicialize a propriedade no construtor
+  }
 
   ngOnInit(): void {
-      
+      this.productService.read().subscribe(products => {
+        this.products = products
+        console.log(products)
+      })
   }
 
 }
