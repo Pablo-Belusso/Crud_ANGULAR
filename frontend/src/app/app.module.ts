dff2bda27168d5a171d26be7b5fa042f5b438316
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-09-05 15:03:26 -0300

    Commit 62

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
 
 
 @NgModule({
@@ -35,7 +36,8 @@ import { MatInputModule } from '@angular/material/input';
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    ProductCreateComponent
+    ProductCreateComponent,
+    ProductReadComponent
   ],
   imports: [
     BrowserModule,
