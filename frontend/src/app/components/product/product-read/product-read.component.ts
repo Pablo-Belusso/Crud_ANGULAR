a273117bb1fc885e37c5f6026cdb6b94cc67cef0
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-09-05 15:31:55 -0300

    Commit 66

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-read',
   templateUrl: './product-read.component.html',
   styleUrls: ['./product-read.component.css']
 })
-export class ProductReadComponent {
+
+export class ProductReadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+      
+  }
 
 }
