d73034d148d47f428b7f67f8309a03a994b1c3e7
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-09-20 19:50:47 -0300

    Commit 85

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -31,4 +31,9 @@ export class ProductService {
     return this.http.get<Product[]>(this.baseUrl)
   }
 
+  readById (id: String): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.get<Product>(url)
+  }
+
 }
