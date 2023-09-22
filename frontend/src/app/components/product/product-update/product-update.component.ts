20e5beef846a28b1fa2f5e5979811552057b6eef
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-09-22 12:06:17 -0300

    Commit 93

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,6 +1,7 @@
 import { Router } from '@angular/router';
 import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-update',
@@ -8,6 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-update.component.css']
 })
 export class ProductUpdateComponent implements OnInit {
+
+  product: Product = {
+    name:'',
+    price: 0
+  }
 
   constructor(private ProductService: ProductService, private router: Router) {}
 
@@ -20,7 +26,7 @@ export class ProductUpdateComponent implements OnInit {
   }
 
   cancel(): void {
-    
+    this.router.navigate(['/products'])
   }
 
 }
