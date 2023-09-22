c322f57763e75ffb6f4523484bc12bb9fb4b08aa
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-09-22 11:49:59 -0300

    Commit 92

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-update',
   templateUrl: './product-update.component.html',
   styleUrls: ['./product-update.component.css']
 })
-export class ProductUpdateComponent {
+export class ProductUpdateComponent implements OnInit {
+
+  constructor(private ProductService: ProductService, private router: Router) {}
+
+  ngOnInit(): void {
+      
+  }
+
+  updateProduct(): void {
+
+  }
+
+  cancel(): void {
+    
+  }
 
 }
