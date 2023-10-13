dea7590f61d22607533c75378add72d2e78da332
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-10-13 14:44:15 -0300

    Commit 147

--- frontend/src/app/views/home/home.component.ts
+++ frontend/src/app/views/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderService } from 'src/app/components/template/header/header.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor () { }
+  constructor (private headerservice: HeaderService) {
+    headerservice.HeaderData
+   }
 
   ngOnInit(): void {
       
